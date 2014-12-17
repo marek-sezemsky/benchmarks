f58bb01ae8fa27d63ce6db362a256f3fcdb39034
Marek Sezemsky <marek.sezemsky@deutsche-boerse.com>
2014-12-17 12:24:41 +0100

    add clearcase-lstype-and-vtree.pl

---
+++ clearcase-lstype-and-vtree.pl
@@ -0,0 +1,70 @@
+#!ratlperl
+#
+# ClearCase timethese: List label types, List version tree
+#
+# Usage: $0 <vobtag> <pname> [<count>]
+#
+# Given vob-tag and pname, measure (via Benchmark::timethese) how long it
+# takes to:
+# - list lbtypes in vobtag (unsorted, default format)
+# - list version tree of pname (show all labels)
+#
+# Will do first iteration, then the fun starts
+#
+# Require 'cleartool' to be present on PATH. Should work with Perl bundled
+# with ClearCase.
+#
+
+use 5.008;
+use strict;
+use warnings;
+
+use Benchmark;
+use File::Spec;
+
+my $_silent = sprintf(">%s", File::Spec->devnull);
+
+sub lbtypes {
+    my $vobtag = shift;
+
+    # ignore retval
+    system("cleartool lstype -kind lbtype -unsorted -invob $vobtag $_silent")
+        and die("failed to lstype in $vobtag");
+}
+
+sub vtree {
+    my $pname = shift;
+
+    # ignore retval
+    system("cleartool lsvtree -all $pname $_silent")
+        and die("failed to lsvtree $pname");
+}
+
+# runround runs the benchmark test (lstype, vtree)
+sub runround {
+    my ($vobtag, $pname) = @_;
+    lbtypes($vobtag);
+    vtree($pname);
+}
+
+
+# main()
+
+my $vobtag = shift;
+my $pname = shift;
+my $count = shift || 10;
+
+die "Usage: $0 <vobtag> <pname> [<count>]" unless ($vobtag and $pname);
+
+# run both actions first to be fair (should populate whatever cache there is)
+warn "dummy iteration";
+lbtypes($vobtag);
+vtree($pname);
+
+timethese(
+    $count,
+    {
+        'List label types' => sub { lbtypes($vobtag) },
+        'List version tree' => sub { vtree($pname) },
+    }
+);
